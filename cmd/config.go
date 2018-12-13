e77d552af33eb65c2f20dd1642099e33139bad17
xuchenCN <chenxu198511@gmail.com>
2018-12-13 11:25:46 +0800

    ssh

--- cmd/config.go
+++ cmd/config.go
@@ -12,6 +12,9 @@ type config struct {
 }
 
 func (c *config) addFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&c.hostFile,"f","","file path of hosts")
+	fs.StringArrayVar(&c.hosts,"h",[]string{},"hosts:ip1,ip2")
+	fs.IntVar(&c.port,"port",22,"port of ssh connect to")
+	fs.StringVar(&c.user,"u","root","user")
 	
-
 }
